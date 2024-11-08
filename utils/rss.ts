47c0e0d2b928b3d0aeafa22902ac67355734a0cc
Lukas Prochazka <prochazka.lu@gmail.com>
2024-11-08 12:19:02 +0100

    Add type

--- utils/rss.ts
+++ utils/rss.ts
@@ -2,7 +2,7 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { Feed } from 'feed';
 import info from '../config/index.json' assert { type: 'json' };
-import { getContentList, IContent, sortByDate } from './content';
+import { getContentList, type IContent, sortByDate } from './content';
 
 const { siteName, siteTitle, siteUrl } = info.site;
 
