e6bdba3f51d1618aabb47376da5ecfe171344406
Karl Horky <karl.horky@gmail.com>
2025-09-05 17:10:09 +0200

    Upgrade to stylelint-config-upleveled@2.0.2, remove config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,14 +1,6 @@
 /** @type {import('stylelint').Config} */
 const config = {
   extends: ['stylelint-config-upleveled'],
-  rules: {
-    'nesting-selector-no-missing-scoping-root': [
-      true,
-      {
-        ignoreAtRules: ['utility'],
-      },
-    ],
-  },
 };
 
 export default config;
