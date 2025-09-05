88a89f606807a9d80d11d0b08630f131e636db78
Karl Horky <karl.horky@gmail.com>
2025-09-05 17:07:26 +0200

    Upgrade to stylelint@16.24.0, remove stylelint-disable comments

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,14 @@
 /** @type {import('stylelint').Config} */
 const config = {
   extends: ['stylelint-config-upleveled'],
+  rules: {
+    'nesting-selector-no-missing-scoping-root': [
+      true,
+      {
+        ignoreAtRules: ['utility'],
+      },
+    ],
+  },
 };
 
 export default config;
